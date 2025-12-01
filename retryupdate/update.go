6b09ff0ee7398d0908bd7640d9eb8f3d5b0bb7a8
Иван Хлебушкин <lbringerl@MacBook-Air-Ivan.local>
2025-12-01 08:54:44 +0300

    retryupdate

--- retryupdate/update.go
+++ retryupdate/update.go
@@ -2,8 +2,84 @@
 
 package retryupdate
 
-import "gitlab.com/slon/shad-go/retryupdate/kvapi"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/gofrs/uuid"
+	"gitlab.com/slon/shad-go/retryupdate/kvapi"
+)
 
 func UpdateValue(c kvapi.Client, key string, updateFn func(oldValue *string) (newValue string, err error)) error {
-	panic("implement me")
+	var (
+		value      *string
+		apiErr     *kvapi.APIError
+		oldVersion = uuid.Nil
+	)
+GetLoop:
+	for {
+		resp, err := c.Get(&kvapi.GetRequest{
+			Key: key,
+		})
+		if err != nil {
+			if errors.Is(err, kvapi.ErrKeyNotFound) {
+				value = nil
+				oldVersion = uuid.Nil
+			} else if errors.As(err, &apiErr) {
+				switch apiErr.Err.(type) {
+				case *kvapi.ConflictError:
+					continue GetLoop
+				case *kvapi.AuthError:
+					return fmt.Errorf("c.Get: %w", err)
+				default:
+					continue GetLoop
+				}
+			}
+		} else {
+			value = &resp.Value
+			oldVersion = resp.Version
+		}
+
+		generatedVersions := make(map[uuid.UUID]struct{})
+
+		for {
+			newValue, err := updateFn(value)
+			if err != nil {
+				return fmt.Errorf("updateFn: %w", err)
+			}
+
+			newVersion := uuid.Must(uuid.NewV4())
+			generatedVersions[newVersion] = struct{}{}
+			_, err = c.Set(&kvapi.SetRequest{
+				Key:        key,
+				Value:      newValue,
+				OldVersion: oldVersion,
+				NewVersion: newVersion,
+			})
+			if err != nil {
+				if errors.Is(err, kvapi.ErrKeyNotFound) {
+					value = nil
+					oldVersion = uuid.Nil
+					continue
+				} else if errors.As(err, &apiErr) {
+					switch apiErr.Err.(type) {
+					case *kvapi.ConflictError:
+						conflictErr := apiErr.Err.(*kvapi.ConflictError)
+						_, exists := generatedVersions[conflictErr.ExpectedVersion]
+						if exists {
+							return nil
+						} else {
+							continue GetLoop
+						}
+					case *kvapi.AuthError:
+						return fmt.Errorf("c.Get: %w", err)
+					default:
+						continue
+					}
+				}
+			}
+
+			return nil
+		}
+	}
 }
