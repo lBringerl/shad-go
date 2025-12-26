d4b319cd90944eda1230058a23b978a83a6d9bc8
Ivan Khlebushkin <lbringerl@hotmail.com>
2025-12-26 21:41:15 +0300

    yamlembed

--- yamlembed/types.go
+++ yamlembed/types.go
@@ -1,19 +1,58 @@
 package yamlembed
 
+import (
+	"strings"
+)
+
 type Foo struct {
-	A string
-	p int64
+	A string `yaml:"aa"`
+	p int64  `yaml:"-"`
 }
 
 type Bar struct {
-	I      int64
-	B      string
-	UpperB string
-	OI     []string
-	F      []any
+	I      int64    `yaml:"-"`
+	B      string   `yaml:"b"`
+	UpperB string   `yaml:"-"`
+	OI     []string `yaml:"oi,omitempty"`
+	F      []any    `yaml:"f,flow"`
+}
+
+type barUnmarshall struct {
+	I      int64    `yaml:"-"`
+	B      string   `yaml:"b"`
+	UpperB string   `yaml:"-"`
+	OI     []string `yaml:"oi,omitempty"`
+	F      []any    `yaml:"f"`
+}
+
+func (b *Bar) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var inner barUnmarshall
+	unmarshal(&inner)
+
+	b.I = inner.I
+	b.B = inner.B
+	b.UpperB = strings.ToUpper(inner.B)
+	b.OI = inner.OI
+	b.F = inner.F
+
+	return nil
 }
 
 type Baz struct {
 	Foo `yaml:",inline"`
 	Bar `yaml:",inline"`
+}
+
+func (b *Baz) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	err := unmarshal(&b.Foo)
+	if err != nil {
+		return err
+	}
+
+	err = b.Bar.UnmarshalYAML(unmarshal)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
