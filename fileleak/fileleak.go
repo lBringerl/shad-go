5902adef55a8e6220546368656d079245b744e17
Ivan Khlebushkin <lbringerl@hotmail.com>
2026-02-14 22:51:59 +0300

    fileleak

--- fileleak/fileleak.go
+++ fileleak/fileleak.go
@@ -2,11 +2,58 @@
 
 package fileleak
 
+import (
+	"fmt"
+	"os"
+)
+
+const procFilesDir = "/proc/self/fd"
+
 type testingT interface {
 	Errorf(msg string, args ...interface{})
 	Cleanup(func())
 }
 
+func getOpenFilesSet() (map[string]struct{}, error) {
+	filesSet := make(map[string]struct{})
+
+	entries, err := os.ReadDir(procFilesDir)
+	if err != nil {
+		return nil, fmt.Errorf("os.ReadDir: %w", err)
+	}
+	for _, entry := range entries {
+		linkVal, err := os.Readlink(fmt.Sprintf("%s/%s", procFilesDir, entry.Name()))
+		if os.IsNotExist(err) {
+			continue
+		} else if err != nil {
+			return nil, fmt.Errorf("os.Readlink: %w", err)
+		}
+		file := fmt.Sprintf("%s->%s", entry.Name(), linkVal)
+		filesSet[file] = struct{}{}
+	}
+
+	return filesSet, nil
+}
+
 func VerifyNone(t testingT) {
-	panic("implement me")
+	openFilesSetStart, err := getOpenFilesSet()
+	if err != nil {
+		panic(fmt.Sprintf("err: %s", err.Error()))
+	}
+
+	compareOpenedFiles := func() {
+		openFilesSetEnd, err := getOpenFilesSet()
+		if err != nil {
+			panic(fmt.Sprintf("err: %s", err.Error()))
+		}
+
+		for file := range openFilesSetEnd {
+			_, exists := openFilesSetStart[file]
+			if !exists {
+				t.Errorf("file leak")
+			}
+		}
+	}
+
+	t.Cleanup(compareOpenedFiles)
 }
