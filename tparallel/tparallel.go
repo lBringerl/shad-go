192a3c05410c6ae782b772c2e859efa1e5d81106
Ivan Khlebushkin <lbringerl@hotmail.com>
2026-02-20 09:49:49 +0300

    tparallel

--- tparallel/tparallel.go
+++ tparallel/tparallel.go
@@ -3,16 +3,81 @@
 package tparallel
 
 type T struct {
+	parallelBarrier chan *T
+	parallelInvoked chan struct{}
+	done            chan struct{}
+	mut             chan struct{}
+
+	parent *T
 }
 
 func (t *T) Parallel() {
-	panic("implement me")
+	t.mut <- struct{}{}
+	<-t.mut
+	t.parallelInvoked <- struct{}{}
+	t.parent.parallelBarrier <- t
+}
+
+func processParallel(t *T) {
+	t.mut <- struct{}{}
+	subtests := make([]*T, 0)
+clearLoop:
+	for {
+		select {
+		case parallelT := <-t.parallelBarrier:
+			subtests = append(subtests, parallelT)
+		default:
+			break clearLoop
+		}
+	}
+	<-t.mut
+	for _, subtest := range subtests {
+		<-subtest.done
+	}
 }
 
 func (t *T) Run(subtest func(t *T)) {
-	panic("implement me")
+	innerT := &T{
+		parallelInvoked: make(chan struct{}),
+		parallelBarrier: t.parallelBarrier,
+		done:            make(chan struct{}),
+		mut:             t.mut,
+
+		parent: t,
+	}
+
+	go func() {
+		defer func() {
+			close(innerT.done)
+		}()
+		subtest(innerT)
+		innerT.done <- struct{}{}
+	}()
+
+	select {
+	case <-innerT.done:
+		processParallel(t)
+	case <-innerT.parallelInvoked:
+		close(innerT.parallelInvoked)
+	}
 }
 
 func Run(topTests []func(t *T)) {
-	panic("implement me")
+	t := &T{
+		parallelBarrier: make(chan *T),
+		parallelInvoked: nil,
+		done:            nil,
+		mut:             make(chan struct{}, 1),
+
+		parent: nil,
+	}
+	defer func() {
+		close(t.parallelBarrier)
+		close(t.mut)
+	}()
+
+	for _, test := range topTests {
+		t.Run(test)
+	}
+	processParallel(t)
 }
